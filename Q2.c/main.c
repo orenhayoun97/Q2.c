2a4bbe92f53ab6cb90352fbd6e2fec9c27409d23
oren hayoun <orenhayoun0@gmail.com>
2022-03-21 20:37:06 +0200

    deleteWorstWorker last function

--- Q2.c/main.c
+++ Q2.c/main.c
@@ -19,8 +19,19 @@ typedef struct Worker {
     }join_year;
 }Worker;
 
+typedef struct WorkerList{
+    Worker* data;
+    struct WorkerList* next;
+}WorkerList;
+
+
+
 Worker* CrateWorker(unsigned long id,char *name,unsigned long salery,void *p2year,int check);
 void PrintWorker(Worker* Worker);
+WorkerList* addWorker(WorkerList *head, Worker* w);
+int index(WorkerList *head, long unsigned id);
+int indexR(WorkerList *head, long unsigned id);
+WorkerList* deleteWorstWorker(WorkerList *head);
 
 int main(){
     
@@ -51,4 +62,84 @@ void PrintWorker(Worker* Worker,int check){ //
     printf("id : %llu\name : %s\nsalery : %llu\njoin year : %llu\n",Worker->id,Worker->name,Worker->salery,Worker->join_year.luz_year);
     }
     else printf("id : %llu\name : %s\nsalery : %llu\njoin year : %s\n",Worker->id,Worker->name,Worker->salery,Worker->join_year.heb_year);
+}
+WorkerList* addWorker(WorkerList *head, Worker* w){
+    WorkerList* New = (WorkerList*) malloc(sizeof(WorkerList));
+    New->data->name = w->name;
+    New->data->salery = w->salery;
+    New->data->join_year = w->join_year;
+    New->data->id = w->id;
+    New->next = NULL;
+    
+    WorkerList* prev = NULL;
+    WorkerList* curr = head;
+    
+    // empty list
+    if(head == NULL){
+        printf("The list is empty...\n");
+        head = New;
+        return head;
+    }
+    if(New->data->salery > prev->data->salery){
+        New->next = head;
+        head = New;
+        return head;
+    }
+    while(curr != NULL && curr->data->salery > New->data->salery){
+        prev=curr;
+        curr=curr->next;
+    }
+    prev->next = New;
+    New->next = curr;
+    return head;
+}
+// Loop
+int index(WorkerList *head, long unsigned id){
+    if(!head){
+        printf("The list is empty\n");
+        return -1;
+    }
+    WorkerList* temp = head;
+    int i = 0,found = 0;
+    do{
+        i++;
+        if(temp->data->id == id){
+            found++;
+        }
+        temp=temp->next;
+    }while(!found || temp == NULL);
+    if(found) return i;
+    else return -1;
+}
+// Recursion
+int indexR(WorkerList *head, long unsigned id){
+    if(head == NULL) return -1;
+    if(head->next == id) return 1;
+    return indexR(head->next,id);
+}
+WorkerList* deleteWorstWorker(WorkerList *head){
+    if(head == NULL){
+        printf("The List is empty...");
+        return head;
+    }
+    WorkerList *lowest = NULL;
+    WorkerList *before = NULL;
+    WorkerList *curr = head->next;
+    WorkerList *prev = head;
+    if(curr == NULL){
+        printf("there is one worker in list...");
+        return head;
+    }
+    while(curr != NULL){
+        if(curr->data->salery > lowest->data->salery){
+            lowest = curr;
+            before = prev;
+        }
+        curr=curr->next;
+        prev=prev->next;
+    }
+    before->next = lowest->next;
+    free(lowest->data->name);
+    free(lowest->next);
+    return head;
 }
